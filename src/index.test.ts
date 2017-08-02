c82474852682ff3f4200e958ac5874f6e7418893
srinivas <srinivasa.gandikota@technoidentity.com>
2017-08-02 10:41:46 +0530

    Programs 1. factorial 2. ncr 3. pascalLine 4. isPrime 5. primesUpTo

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,34 @@
-import add from './index';
+import { is, List } from 'immutable';
+import { factorial, isPrime, ncr, pascalLine, primesUpTo } from './index';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+test('factorial', async () => {
+  expect(factorial(0)).toBe(1);
+  expect(factorial(1)).toBe(1);
+  expect(factorial(5)).toBe(120);
 });
+
+test('ncr', () => {
+  expect(ncr(5, 0)).toBe(1);
+  expect(ncr(5, 5)).toBe(1);
+  expect(ncr(5, 4)).toBe(5);
+});
+
+
+test('pascalLine', () => {
+  expect(is(pascalLine(1), List([1]))).toBeTruthy();
+  expect(is(pascalLine(2), List([1, 1]))).toBeTruthy();
+  expect(is(pascalLine(3), List([1, 2, 1]))).toBeTruthy();
+  expect(is(pascalLine(4), List([1, 3, 3, 1]))).toBeTruthy();
+});
+
+test('isPrime', () => {
+  expect(isPrime(5)).toBeTruthy();
+  expect(isPrime(4)).toBeFalsy();
+});
+
+test('primeNumbers in 5 to be [2,3,5]', () => {
+  expect(is(primesUpTo(5), List([2, 3, 5]))).toBe(true);
+  expect(is(primesUpTo(10), List([2, 3, 5, 7]))).toBe(true);
+})
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'sourcemap',
-  output: {
-    publicPath: "/build/",
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader' },
-      { test: /\.ts$/, loader: "tslint-loader" },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  devServer: {
-    open: true,
-    openPage: "index.html",
-    inline: true,
-    noInfo: true,
-    port: 3000,
-  },
+    entry: './src/index.ts',
+    devtool: 'sourcemap',
+    output: {
+        publicPath: "/build/",
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' },
+            { test: /\.ts$/, loader: "tslint-loader" },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    devServer: {
+        open: true,
+        openPage: "index.html",
+        inline: true,
+        noInfo: true,
+        port: 4000,
+    },
 };
